1ab7d95298d0409fdc70909b27f5619018893612
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2024-02-20 20:33:25 +0100

    detalle update

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.querySelector(".topbar").style.backgroundColor = "#111";
-    } else {
-        document.querySelector(".topbar").style.backgroundColor = "#333";
-    }
-}
