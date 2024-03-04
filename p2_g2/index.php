747f5514f748052fb9729bd99b8b5ae8d3e6eb51
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2024-03-04 23:54:26 +0100

    Update index.php

---
+++ p2_g2/index.php
@@ -0,0 +1,14 @@
+<!-- primer formato de html -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Principal</title>
+    <link rel="icon" href="img/logo.jpg" type="image/png">
+    <link rel="stylesheet" href="CSS/cabecera.css">
+</head>
+<body>
+    <?php
+        require("cabecera.php");
+    ?>
