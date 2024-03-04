91bd631edcfa524ecd5487a7fc5c667baaa46b12
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2024-03-04 23:29:07 +0100

    .

--- p2_g2/includes/vistas/comun/cabecera.php
+++ p2_g2/includes/vistas/comun/cabecera.php
@@ -1,3 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introdución del Proyecto</title>
+    <link rel="icon" href="img/logo.jpg" type="image/png">
+    <link rel="stylesheet" href="CSS/cabecera.css">
+</head>
+<body>
+    <?php
+    <!-- topbar -->
+    <div class="topbar">
+
+        <!-- Learnique -->
+        <div class="topbar-name">
+            <a class="topbar-name">Learnique</a>
+        </div>
+
+        <!-- topbar items -->
+        <div>
+            <a href="index.html" class="topbar-item">Inicio</a>
+            <a href="detalles.html" class="topbar-item">Detalles</a>
+            <a href="bocetos.html" class="topbar-item">Bocetos</a>
+            <a href="miembros.html" class="topbar-item">Miembros</a>
+            <a href="planificacion.html" class="topbar-item">Planificación</a>
+            <a href="contacto.html" class="topbar-item">Contacto</a>
+        </div>
+        
+
+    </div>
+
+    <div class="intro">
+        <figure class="logo">
+            <img src="img/logo.jpg" alt="Logo de Learnique">
+        </figure>
+    </div>
+    
+</body>
+
+</html>
+
+
+
 <?php
     // <!-- topbar items -->
     <div>
