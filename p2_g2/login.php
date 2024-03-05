9b6b645bc3dd1f1c6d15483ad1cfc1e121b0af3c
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2024-03-05 01:22:10 +0100

    registro + login (solo visualización)

---
+++ p2_g2/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Pagina Principal</title>
+        <link rel="stylesheet" href="CSS/topBar.css">
+        <link rel="stylesheet" href="CSS/registro_login.css">
+    </head>
+    <body>
+    
+        <!-- topbar -->
+        <div class="topbar">
+            <!-- Learnique -->
+            <div class="topbar-name">
+                <a class="topbar-name">Learnique</a>
+            </div>
+            <!-- topbar items -->
+            <div>
+                <a href="index.php" class="topbar-item">Inicio</a>
+                <a href="login.php" class="topbar-item">Log In</a>
+                <a href="registro.php" class="topbar-item">Registro</a>
+                <a href="cursos.php" class="topbar-item">Cursos</a>
+            </div>
+        </div>
+        
+        <div class="formularios">
+
+            <h1>Log In</h1>
+
+            <form method="post" action="" name="signin-form">
+                <div class="form-element">
+                    <label>Username</label>
+                    <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+                </div>
+                <div class="form-element">
+                    <label>Password</label>
+                    <input type="password" name="password" required />
+                </div>
+                <button type="submit" name="login" value="login">Log In</button>
+            </form>
+
+        </div>
+    </body>
+
+</html>
+
+        </div>
+    </body>
+
+</html>
