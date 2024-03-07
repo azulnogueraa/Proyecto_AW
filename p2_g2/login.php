1e37112f90ff1561cc85e025e1b70a5463405d23
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2024-03-07 19:29:40 +0100

    login arreglo

--- p2_g2/login.php
+++ p2_g2/login.php
@@ -15,7 +15,7 @@ session_start();
         <!-- topbar -->
         <?php require "includes/vistas/comun/topbar.php"; ?>
 
-     <main>
+        <main>
             <article>
                 <h1>Log In</h1>
                 <form method="post" action="procesarLogin.php" name="signin-form">
@@ -23,22 +23,21 @@ session_start();
                     <div class="legenda">
                         <legend>Datos para reingresar</legend>
                     </div>
-                    <div>
+                    <div class="form-element">
                         <label for="nombreUsuario">Nombre de usuario:</label>
-                        <input id="nombreUsuario" type="text" name="nombreUsuario" />
+                        <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
                     </div>
-                    <div>
+                    <div class="form-element">
                         <label for="password">Contraseña:</label>
-                        <input id="password" type="password" name="password" />
+                        <input type="password" name="password" required />
                     </div>
                     <div class="boton">
-                        <button type="submit" name="registro">Log In</button>
+                        <button type="submit" name="login" value="login">Log In</button>
                     </div>
                 </fieldset>
                 </form>
             </article>
-        </main> 
-        
+        </main>
     </body>
 
 </html>
