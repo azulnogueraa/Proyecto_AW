b092b46cbafb78fab8489b4abe17b6221ed9e29b
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2024-03-08 13:58:56 +0100

    update tablas

--- p2_g2/mysql/tablas.sql
+++ p2_g2/mysql/tablas.sql
@@ -1,40 +1,5 @@
-"DROP TABLE IF EXISTS `Usuarios`;"
-"DROP TABLE IF EXISTS `Cursos`;"
-
-'''
-nombre_usuario: El nombre de usuario único que actuará como clave única. 
-nombre: El nombre del usuario.
-apellido: El apellido del usuario.
-email: El correo electrónico del usuario, que también se define como único.
-contraseña: La contraseña del usuario, almacenada de manera segura.
-rol: El rol del usuario en el sistema (Estudiante, Profesor o Administrador).
-'''
-
-USE Learnique;
-CREATE TABLE IF NOT EXISTS Usuarios (
-    nombre_usuario VARCHAR(100) PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    contraseña VARCHAR(255) NOT NULL,
-    rol ENUM('Estudiante', 'Profesor', 'Administrador') NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-'''
-nombre_curso: El nombre del curso, establecido como clave primaria.
-descripcion: Una descripción detallada del curso.
-profesor_id: El ID del usuario que es el profesor o instructor a cargo del curso.
-fecha_creacion: La fecha en que el curso fue creado.
-duracion: La duración estimada del curso.
-nivel_dificultad: El nivel de dificultad del curso.
-categoria: La categoría o área temática del curso.
-requisitos_previos: Requisitos previos necesarios para tomar el curso.
-precio: El precio del curso.
-estado_curso: El estado actual del curso.
-'''
-
 create table if not exists Estudiante (
-    id int,
+    id int auto_increment,
     nombre_usuario varchar(100),
     apellido varchar(100),
     email varchar(100),
@@ -43,7 +8,7 @@ create table if not exists Estudiante (
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 create table if not exists Profesor (
-    id int,
+    id int auto_increment,
     nombre_usuario varchar(100),
     apellido varchar(100),
     email varchar(100),
@@ -53,7 +18,7 @@ create table if not exists Profesor (
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 create table if not exists Administrador (
-    id int,
+    id int auto_increment,
     nombre_usuario varchar(100),
     apellido varchar(100),
     email varchar(100),
