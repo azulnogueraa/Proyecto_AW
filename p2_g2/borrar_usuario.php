3d71901ca0716ea6e9244628dbe83a776335ed2a
Patricio Guledjian <patogulee@gmail.com>
2024-03-08 17:41:25 +0100

    ajustes nuevo

--- p2_g2/borrar_usuario.php
+++ p2_g2/borrar_usuario.php
@@ -1,31 +1,29 @@
 <?php
-    session_start();
+session_start();
+require_once "includes/utils.php"; // Incluye el archivo de utilidades para la conexión a la base de datos
+$mysqli = conexionBD(); // Establece la conexión a la base de datos
 
-    // Verifica si se envió el formulario y si se proporcionó un nombre de usuario para eliminar
-    if (isset($_POST["borrar"]) && isset($_POST["usuario"])) {
-        // Incluye el archivo de utilidades para la conexión a la base de datos
-        require_once "includes/utils.php";
-        // Establece la conexión a la base de datos
-        $mysqli = conexionBD();
-        
-        // Sanitiza y escapa el nombre de usuario
-        $usuario = $mysqli->real_escape_string($_POST["usuario"]);
-        
-        // Construye y ejecuta la consulta para borrar el usuario de las tablas correspondientes
-        $sql = "DELETE FROM Estudiante WHERE nombre_usuario = '$usuario';
-                DELETE FROM Profesor WHERE nombre_usuario = '$usuario';
-                DELETE FROM Administrador WHERE nombre_usuario = '$usuario'";
-        if ($mysqli->multi_query($sql)) {
-            echo "El usuario '$usuario' ha sido eliminado correctamente.";
-        } else {
-            echo "Error al intentar borrar el usuario.";
-        }
+// Verifica si se envió el formulario y si se proporcionó un nombre de usuario para eliminar
+if (isset($_POST["borrar"]) && isset($_POST["usuario"])) {
+    // Sanitiza y escapa el nombre de usuario
+    $usuario = $mysqli->real_escape_string($_POST["usuario"]);
 
-        // Cierra la conexión a la base de datos
-        $mysqli->close();
+    // Construye y ejecuta la consulta para borrar el usuario de las tablas correspondientes
+    $sql = "DELETE FROM Estudiante WHERE nombre_usuario = '$usuario';
+            DELETE FROM Profesor WHERE nombre_usuario = '$usuario';
+            DELETE FROM Administrador WHERE nombre_usuario = '$usuario'";
+    if ($mysqli->multi_query($sql)) {
+        $_SESSION['mensaje'] = "El usuario '$usuario' ha sido eliminado correctamente.";
     } else {
-        // Si no se envió el formulario correctamente, redirige a alguna página adecuada
-        header("Location: ajustes.php");
-        exit();
+        $_SESSION['mensaje'] = "Error al intentar borrar el usuario.";
     }
+
+    // Redirige de vuelta a ajustes.php
+    header("Location: ajustes.php");
+    exit();
+} else {
+    // Si no se envió el formulario correctamente, redirige a ajustes.php
+    header("Location: ajustes.php");
+    exit();
+}
 ?>
