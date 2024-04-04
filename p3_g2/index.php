c283e0e7081b277d0e02ae535beb2696e0771295
azulnogueraa <anoguera@mail.utdt.edu>
2024-04-04 16:28:42 +0200

    cambios en el index

--- p3_g2/index.php
+++ p3_g2/index.php
@@ -17,15 +17,38 @@ session_start();
     <!-- topbar -->
     <?php require "includes/vistas/comun/topbar.php"; ?>
 
-
-    <div class="texto"> 
-      <h1> Cursos Online para transformar tu realidad en tiempo récord</h1>
-      <p>Clases en línea y en vivo dictadas por referentes de la industria, 
-        enfoque 100% práctico, mentorías personalizadas y acceso a una gran 
-        comunidad de estudiantes.</p>
-      <button onclick="location.href='cursos.php';" type="submit" name="cursos" value="cursos">Ver todos los cursos</button>
-
+    <div class="caja">
+      <div class="texto"> 
+        <h1> Cursos Online para transformar tu realidad en tiempo récord</h1>
+        <p>Clases en línea y en vivo dictadas por referentes de la industria, 
+          enfoque 100% práctico, mentorías personalizadas y acceso a una gran 
+          comunidad de estudiantes.</p>
+        <button onclick="location.href='cursos.php';" type="submit" name="cursos" value="cursos">Ver todos los cursos</button>
+      </div>
     </div>
+
+    <hr>
+
+    <div class="alumnos">
+      <img class="alumni" src="img/chat_alumnos.png" alt="">
+      <div class="info">
+        <h2> Aprende junto a tus compañeros </h2>
+        <p>Está demostrado que aprender en grupo es más eficiente y motivador. 
+        El networking con tus compañeros de clase ayuda a que puedas tener nuevas 
+        ideas y hacer mejores proyectos. </p>
+      </div>  
+      
+    </div>
+
+    <div class="profesores">
+      <div>
+        <p> texto o imagen despues se ve </p>
+      </div>
+      <div>
+      <img class="profe" src="img/chat_profe.png" alt="">
+      </div>
+    </div>
+
 
   </body>
 </html>
