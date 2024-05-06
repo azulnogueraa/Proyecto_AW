e648a035d4ed386d71ef8e226969b49d5da941e0
azulnogueraa <anoguera@mail.utdt.edu>
2024-05-06 17:18:24 +0200

    mismo que antes

--- proyecto_final/includes/src/Aplicacion.php
+++ proyecto_final/includes/src/Aplicacion.php
@@ -9,70 +9,95 @@ class Aplicacion {
 
     private static $instancia;
     /**
-	 * @var array Almacena los datos de configuración de la BD
-	 */
+     * @var array Almacena los datos de configuración de la BD
+     */
     private $bdDatosConexion;
     /**
-	 * Almacena si la Aplicacion ya ha sido inicializada.
-	 * @var boolean
-	 */
+     * Almacena si la Aplicacion ya ha sido inicializada.
+     * @var boolean
+     */
     private $inicializada = false;
     /**
-	 * @var \mysqli Conexión de BD.
-	 */
-	private $conn;
+     * @var \mysqli Conexión de BD.
+     */
+    private $conn;
 
+    /**
+     * Constructor privado para evitar la creación directa de instancias
+     */
     private function __construct() {}
 
+    /**
+     * Devuelve la instancia singleton de la clase
+     * 
+     * @return self La instancia única de la clase
+     */
     public static function getInstance() {
         if (!self::$instancia instanceof self) {
-			self::$instancia = new static();
-		}
-		return self::$instancia;
+            self::$instancia = new static();
+        }
+        return self::$instancia;
     }
 
+    /**
+     * Inicializa la aplicación con los datos de conexión a la base de datos
+     * 
+     * @param array $bdDatosConexion Datos de conexión a la base de datos
+     */
     public function init($bdDatosConexion) {
-        if ( ! $this->inicializada ) {
-    	    $this->bdDatosConexion = $bdDatosConexion;
-    		$this->inicializada = true;
-    		session_start();
+        if (!$this->inicializada) {
+            $this->bdDatosConexion = $bdDatosConexion;
+            $this->inicializada = true;
+            session_start();
         }
     }
 
+    /**
+     * Cierra la conexión a la base de datos si está abierta
+     */
     public function shutdown() {
-	    $this->compruebaInstanciaInicializada();
-	    if ($this->conn !== null && ! $this->conn->connect_errno) {
-	        $this->conn->close();
-	    }
-	}
+        $this->compruebaInstanciaInicializada();
+        if ($this->conn !== null && !$this->conn->connect_errno) {
+            $this->conn->close();
+        }
+    }
 
+    /**
+     * Comprueba si la aplicación ha sido inicializada
+     */
     private function compruebaInstanciaInicializada() {
-	    if (! $this->inicializada ) {
-	        echo "Aplicacion no inicializa";
-	        exit();
-	    }
-	}
+        if (!$this->inicializada) {
+            echo "Aplicación no inicializada. Por favor, llame al método init().";
+            exit();
+        }
+    }
 
-    public function getConexionBd()
-	{
-	    $this->compruebaInstanciaInicializada();
-		if (! $this->conn ) {
-			$bdHost = $this->bdDatosConexion['host'];
-			$bdUser = $this->bdDatosConexion['user'];
-			$bdPass = $this->bdDatosConexion['pass'];
-			$bd = $this->bdDatosConexion['bd'];
-			
-			$conn = new \mysqli($bdHost, $bdUser, $bdPass, $bd);
-			if ( $conn->connect_errno ) {
-				echo "Error de conexión a la BD ({$conn->connect_errno}):  {$conn->connect_error}";
-				exit();
-			}
-			if ( ! $conn->set_charset("utf8mb4")) {
-				echo "Error al configurar la BD ({$conn->errno}):  {$conn->error}";
-				exit();
-			}
-			$this->conn = $conn;
-		}
-		return $this->conn;
-	}
+    /**
+     * Devuelve la conexión a la base de datos
+     * 
+     * @return \mysqli La conexión a la base de datos
+     */
+    public function getConexionBd() {
+        $this->compruebaInstanciaInicializada();
+        if (!$this->conn) {
+            $bdHost = $this->bdDatosConexion['host'];
+            $bdUser = $this->bdDatosConexion['user'];
+            $bdPass = $this->bdDatosConexion['pass'];
+            $bd = $this->bdDatosConexion['bd'];
+
+            $conn = new \mysqli($bdHost, $bdUser, $bdPass, $bd);
+            if ($conn->connect_errno) {
+                error_log("Error de conexión a la BD ({$conn->connect_errno}): {$conn->connect_error}");
+                echo "Error de conexión a la base de datos. Por favor, intente más tarde.";
+                exit();
+            }
+            if (!$conn->set_charset("utf8mb4")) {
+                error_log("Error al configurar la BD ({$conn->errno}): {$conn->error}");
+                echo "Error al configurar la base de datos. Por favor, intente más tarde.";
+                exit();
+            }
+            $this->conn = $conn;
+        }
+        return $this->conn;
+    }
 }
--- proyecto_final/includes/src/Curso.php
+++ proyecto_final/includes/src/Curso.php
@@ -1,15 +1,14 @@
 <?php
 namespace es\ucm\fdi\aw;
 use mysqli_sql_exception;
-class Curso {
 
-    private $precio;
+class Curso {
     private $nombre_curso;
+    private $precio;
     private $descripcion;
     private $duracion;
     private $categoria;
     private $nivel_dificultad;
-    // private $lista_alumnos;
 
     public function __construct($nombre, $precio, $descripcion, $duracion, $categoria, $nivel_dificultad) {
         $this->nombre_curso = $nombre;
@@ -20,210 +19,216 @@ class Curso {
         $this->nivel_dificultad = $nivel_dificultad;
     }
 
-    public static function crea($nombre, $precio, $descripcion, $duracion, $categoria, $nivel_dificultad){
+    /**
+     * Crea un nuevo curso y lo inserta en la base de datos.
+     */
+    public static function crea($nombre, $precio, $descripcion, $duracion, $categoria, $nivel_dificultad) {
         $curso = new Curso($nombre, $precio, $descripcion, $duracion, $categoria, $nivel_dificultad);
         return self::inserta($curso);
-
     }
 
-    private static function inserta($curso){
-        $result = false;
+    /**
+     * Inserta un curso en la base de datos.
+     */
+    private static function inserta($curso) {
         $conn = Aplicacion::getInstance()->getConexionBd();
-        $query=sprintf("INSERT INTO Curso(nombre_curso, precio, descripcion, duracion, categoria, nivel_dificutad) VALUES ('%s', '%s', '%s')"
-            , $conn->real_escape_string($curso->nombre_curso)                  
-            , $conn->real_escape_string($curso->precio)
-            , $conn->real_escape_string($curso->descripcion)
-            , $conn->real_escape_string($curso->duracion)
-            , $conn->real_escape_string($curso->categoria)
-            , $conn->real_escape_string($curso->nivel_dificultad)
+        $query = sprintf(
+            "INSERT INTO Curso (nombre_curso, precio, descripcion, duracion, categoria, nivel_dificultad) 
+            VALUES ('%s', '%s', '%s', '%s', '%s', '%s')",
+            $conn->real_escape_string($curso->nombre_curso),
+            $conn->real_escape_string($curso->precio),
+            $conn->real_escape_string($curso->descripcion),
+            $conn->real_escape_string($curso->duracion),
+            $conn->real_escape_string($curso->categoria),
+            $conn->real_escape_string($curso->nivel_dificultad)
         );
-        $res = $conn->query($query);//ponerlo en un if para seguridad y exponer un mensaje
+        $result = $conn->query($query);
         return $result !== false;
     }
 
+    /**
+     * Crea un nuevo curso asignándole un profesor y una fecha de creación.
+     */
     public static function crearCurso($nombre, $descripcion, $profesorId, $duracion, $nivelDificultad, $categoria, $precio) {
         $conn = Aplicacion::getInstance()->getConexionBd();
-    
         $query = "INSERT INTO Curso (nombre_curso, descripcion, profesor_id, duracion, nivel_dificultad, categoria, precio, fecha_creacion) 
                   VALUES (?, ?, ?, ?, ?, ?, ?, current_timestamp())";
-    
         $stmt = $conn->prepare($query);
         $stmt->bind_param("ssissss", $nombre, $descripcion, $profesorId, $duracion, $nivelDificultad, $categoria, $precio);
-
         $result = $stmt->execute();
-    
         if ($result === false) {
             error_log("Error al insertar curso: " . $stmt->error);
             return false;
         }
-    
         $stmt->close();
-    
         return true;
     }
 
+    /**
+     * Busca un curso en la base de datos por su nombre.
+     */
     public static function buscaCursoPorNombre($nombreCurso) {
-        // Obtener la conexión a la base de datos desde la aplicación
         $conn = Aplicacion::getInstance()->getConexionBd();
-
-        // Preparar la consulta SQL para buscar un curso por su nombre
         $query = "SELECT * FROM Curso WHERE nombre_curso = ?";
         $stmt = $conn->prepare($query);
         $stmt->bind_param("s", $nombreCurso);
         $stmt->execute();
         $result = $stmt->get_result();
-
         if ($result && $result->num_rows > 0) {
             $row = $result->fetch_assoc();
-            $curso = new Curso($row['nombre_curso'], $row['descripcion'], $row['duracion'], $row['nivel_dificultad'], $row['categoria'], $row['precio']);
-            return $curso;
+            return new Curso(
+                $row['nombre_curso'],
+                $row['precio'],
+                $row['descripcion'],
+                $row['duracion'],
+                $row['categoria'],
+                $row['nivel_dificultad']
+            );
         }
-
-        return null; // Si no se encuentra ningún curso con ese nombre
+        return null;
     }
-    
 
+    /**
+     * Obtiene el nombre del curso.
+     */
     public function getNombre() {
         return $this->nombre_curso;
     }
+
+    /**
+     * Obtiene el precio del curso.
+     */
     public function getPrecio() {
         return $this->precio;
     }
+
+    /**
+     * Obtiene la descripción del curso.
+     */
     public function getDescripcion() {
         return $this->descripcion;
     }
 
+    /**
+     * Obtiene la categoría del curso.
+     */
     public function getCategoria() {
         return $this->categoria;
     }
 
+    /**
+     * Obtiene la duración del curso.
+     */
     public function getDuracion() {
         return $this->duracion;
     }
 
+    /**
+     * Obtiene el nivel de dificultad del curso.
+     */
     public function getNivelDificultad() {
         return $this->nivel_dificultad;
-    }    
+    }
+
+    /**
+     * Obtiene todos los cursos disponibles.
+     */
     static public function obtenerCursos() {
         $conn = Aplicacion::getInstance()->getConexionBd();
-        $query = sprintf("SELECT * FROM Curso");
+        $query = "SELECT * FROM Curso";
         $rs = $conn->query($query);
-        $result = false;
         if ($rs) {
             $result = [];
             while ($row = $rs->fetch_assoc()) {
-                $curso = new Curso(
+                $result[] = new Curso(
                     $row['nombre_curso'],
                     $row['precio'],
                     $row['descripcion'],
                     $row['duracion'],
                     $row['categoria'],
-                    $row['nivel_dificultad']);
-                $result[] = $curso;
+                    $row['nivel_dificultad']
+                );
             }
             $rs->free();
-        } else {
-            error_log("Error BD ({$conn->errno}): {$conn->error}");
+            return $result;
         }
-        return $result;
+        error_log("Error BD ({$conn->errno}): {$conn->error}");
+        return false;
     }
 
+    /**
+     * Obtiene una lista de los nombres de todos los cursos disponibles.
+     */
     static public function obtenerNombreCursos() {
         $conn = Aplicacion::getInstance()->getConexionBd();
-        $query=sprintf("SELECT nombre_curso FROM Curso");
+        $query = "SELECT nombre_curso FROM Curso";
         $rs = $conn->query($query);
-        $result = false;
         if ($rs) {
             $result = [];
             while ($row = $rs->fetch_assoc()) {
                 $result[] = $row["nombre_curso"];
             }
             $rs->free();
-        } else {
-            error_log("Error BD ({$conn->errno}): {$conn->error}");
+            return $result;
         }
-        return $result;
+        error_log("Error BD ({$conn->errno}): {$conn->error}");
+        return false;
     }
 
+    /**
+     * Edita un curso existente en la base de datos.
+     */
     public static function editarCurso($nombreCurso) {
         $conn = Aplicacion::getInstance()->getConexionBd();
-
-        // Consulta para obtener la información del curso por su nombre
         $query = "SELECT * FROM Curso WHERE nombre_curso = ?";
         $stmt = $conn->prepare($query);
         $stmt->bind_param("s", $nombreCurso);
         $stmt->execute();
         $result = $stmt->get_result();
-
-        // Verificar si se encontró el curso
         if ($result->num_rows > 0) {
-            // Obtener los datos del curso como un array asociativo
-            $curso = $result->fetch_assoc();
-            return $curso; // Devolver los datos del curso
-        } else {
-            // Curso no encontrado, lanzar una excepción o devolver null
-            return null;
+            return $result->fetch_assoc();
         }
+        return null;
     }
 
-    public function setDescripcion($descripcion) {
-        $this->descripcion = $descripcion;
-    }
-
-    public function setDuracion($duracion) {
-        $this->duracion = $duracion;
-    }
-
-    public function setNivelDificultad($nivel_dificultad) {
-        $this->nivel_dificultad = $nivel_dificultad;
-    }
-
-    public function setCategoria($categoria) {
-        $this->categoria = $categoria;
-    }
-
-    public function setPrecio($precio) {
-        $this->precio = $precio;
-    }
-
+    /**
+     * Actualiza la información del curso actual en la base de datos.
+     */
     public function actualizarCurso() {
         $conn = Aplicacion::getInstance()->getConexionBd();
         $sql = "UPDATE Curso SET descripcion=?, duracion=?, nivel_dificultad=?, categoria=?, precio=? WHERE nombre_curso=?";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("ssssds", $this->descripcion, $this->duracion, $this->nivel_dificultad, $this->categoria, $this->precio, $this->nombre_curso);
-
         $result = $stmt->execute();
         $stmt->close();
         return $result;
     }
 
+    /**
+     * Borra un curso de la base de datos por su nombre.
+     */
     public static function borrarCurso($nombreCurso) {
         $conn = Aplicacion::getInstance()->getConexionBd();
-
-        // Consulta SQL para eliminar el curso por su nombre
         $query = "DELETE FROM Curso WHERE nombre_curso = ?";
         $stmt = $conn->prepare($query);
         $stmt->bind_param("s", $nombreCurso);
-
-        // Ejecutar la consulta y verificar el resultado
         $result = $stmt->execute();
         $stmt->close();
-
         return $result;
     }
 
+    /**
+     * Verifica si un profesor tiene cursos asignados.
+     */
     public static function cursosDelProfe($idProfe) {
         $conn = Aplicacion::getInstance()->getConexionBd();
-    
         try {
             $query = "SELECT COUNT(*) FROM Curso WHERE profesor_id = ?";
             $stmt = $conn->prepare($query);
             $stmt->bind_param("i", $idProfe);
             $stmt->execute();
-    
             $stmt->bind_result($numCursos);
             $stmt->fetch();
-    
             return $numCursos > 0;
         } catch (mysqli_sql_exception $e) {
             error_log("Error al contar cursos del profesor: " . $e->getMessage());
@@ -231,23 +236,54 @@ class Curso {
         }
     }
 
+    /**
+     * Verifica si un curso tiene alumnos inscritos.
+     */
     public function tieneAlumnosInscritos() {
-        // Obtener la conexión a la base de datos desde la aplicación
         $conn = Aplicacion::getInstance()->getConexionBd();
-
-        // Preparar la consulta SQL para contar los registros de alumnos inscritos en este curso
         $query = "SELECT COUNT(*) as total FROM Registrado WHERE curso_id = ?";
         $stmt = $conn->prepare($query);
         $stmt->bind_param("s", $this->nombre_curso);
         $stmt->execute();
         $result = $stmt->get_result();
-
         if ($result && $row = $result->fetch_assoc()) {
-            $total_alumnos_inscritos = $row['total'];
-            return $total_alumnos_inscritos > 0; // Devuelve true si hay alumnos inscritos, false si no
+            return $row['total'] > 0;
         }
-
-        return false; 
+        return false;
     }
 
+    /**
+     * Establece una nueva descripción para el curso.
+     */
+    public function setDescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+    }
+
+    /**
+     * Establece una nueva duración para el curso.
+     */
+    public function setDuracion($duracion) {
+        $this->duracion = $duracion;
+    }
+
+    /**
+     * Establece un nuevo nivel de dificultad para el curso.
+     */
+    public function setNivelDificultad($nivel_dificultad) {
+        $this->nivel_dificultad = $nivel_dificultad;
+    }
+
+    /**
+     * Establece una nueva categoría para el curso.
+     */
+    public function setCategoria($categoria) {
+        $this->categoria = $categoria;
+    }
+
+    /**
+     * Establece un nuevo precio para el curso.
+     */
+    public function setPrecio($precio) {
+        $this->precio = $precio;
+    }
 }
