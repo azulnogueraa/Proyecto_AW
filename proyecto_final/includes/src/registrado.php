ada45bd67968887795b1ab19706b5f4a3750e405
Patricio Guledjian <patogulee@gmail.com>
2024-05-08 15:34:26 +0200

    Revert "formulario inscripcion do not access to DB"
    This reverts commit 2e0da797033288fc18338fd4dfc7e212c955a694.

--- proyecto_final/includes/src/registrado.php
+++ proyecto_final/includes/src/registrado.php
@@ -6,52 +6,52 @@ require_once __DIR__.'/Usuario.php'; // Asegúrate de incluir el archivo de la c
 class Registrado {
 
     private $u_id;
-    private $nombre_curso;
+    private $curso_id;
 
     public function __construct($usuario, $curso) {
         $this->u_id = $usuario;
-        $this->nombre_curso = $curso;
+        $this->curso_id = $curso;
     }
 
     public static function crea($usuario, $curso){
-        $id_usuario = $usuario->getId(); // Utiliza la flecha ->
+        $nombre_usuario = $usuario->getNombreUsuario(); // Utiliza la flecha ->
         $nombre_curso = $curso->getNombre(); // Utiliza la flecha ->
-        $registrado = new Registrado($id_usuario, $nombre_curso);
+        $registrado = new Registrado($nombre_usuario, $nombre_curso);
         return self::inserta($registrado);
     }
 
     private static function inserta($registrado){
         $result = false;
         $conn = Aplicacion::getInstance()->getConexionBd();
-        $queryCheck = sprintf("SELECT COUNT(*) as count FROM Registrado WHERE u_id = '%d' AND curso_id = '%s'",
-                                $conn->real_escape_string($registrado->u_id),
-                                $conn->real_escape_string($registrado->nombre_curso)
-                                );
+        $queryCheck = sprintf("SELECT COUNT(*) as count FROM Registrado WHERE u_id = '%s' AND curso_id = '%s'",
+        $conn->real_escape_string($registrado->u_id),
+        $conn->real_escape_string($registrado->curso_id)
+    );
 
-        $resCheck = $conn->query($queryCheck);
-        $row = $resCheck->fetch_assoc();
+    $resCheck = $conn->query($queryCheck);
+    $row = $resCheck->fetch_assoc();
 
-        if ($row['count'] == 0) { // If the pair does not exist, proceed with insertion
-            $query = sprintf("INSERT INTO Registrado(u_id, curso_id) VALUES ('%d', '%s')",
-                $conn->real_escape_string($registrado->u_id),
-                $conn->real_escape_string($registrado->nombre_curso)
-            );
+    if ($row['count'] == 0) { // If the pair does not exist, proceed with insertion
+        $query = sprintf("INSERT INTO Registrado(u_id, curso_id) VALUES ('%s', '%s')",
+            $conn->real_escape_string($registrado->u_id),
+            $conn->real_escape_string($registrado->curso_id)
+        );
 
-            $res = $conn->query($query);
-            $result = $res !== false;
-        } else {
-            // Pair already exists in the database
-            echo "Ya estas registrado para este curso";
-        }
-
-        return $result;
+        $res = $conn->query($query);
+        $result = $res !== false;
+    } else {
+        // Pair already exists in the database
+        echo "Ya estas registrado para este curso";
     }
+
+    return $result;
+}
 
     public function getUsuario() {
         return $this->u_id;
     }
     public function getCurso() {
-        return $this->nombre_curso;
+        return $this->curso_id;
     }
 }
 ?>
